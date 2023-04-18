e1f4e3f4f52db40aaab1e190358e2dd10b741841
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-04-18 09:23:54 +0200

    brand-checkbox-functionality-added

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
-const fetchedProductsMap = new Map();
+let fetchedProductsMap = new Map();
 let cartProducts = [];
-const brandSet = new Set();
-const productSet = new Set();
-const brandTotalMap = new Map();
-const isBrandCheckedSet = new Set();
+let brandSet = new Set();
+let productSet = new Set();
+let brandTotalMap = new Map();
+let isBrandCheckedSet = new Set();
+
+// if (window.localStorage.getItem("fetchedProductMapJSON")) {
+//   fetchedProductsMap = JSONtoMap(window.localStorage.getItem("fetchedProductMapJSON"));
+// }
+// if (window.localStorage.getItem("cartProductsJSON")) {
+//   cartProducts = JSON.parse(window.localStorage.getItem("cartProductsJSON"));
+// }
+// if (window.localStorage.getItem("brandSetJSON")) {
+//   brandSet = JSONtoSet(window.localStorage.getItem("brandSetJSON"));
+// }
+// if (window.localStorage.getItem("productSetJSON")) {
+//   productSet = JSONtoSet(window.localStorage.getItem("productSetJSON"));
+// }
+// if (window.localStorage.getItem("brandTotalMapJSON")) {
+//   brandTotalMap = JSONtoMap(window.localStorage.getItem("brandTotalMapJSON"));
+// }
+// if (window.localStorage.getItem("brandTotalMapJSON")) {
+//   isBrandCheckedSet = JSONtoSet(window.localStorage.getItem("isBrandCheckedSetJSON"));
+// }
+
+window.addEventListener("load", async (e) => {
+  const response = await fetch("https://dummyjson.com/products");
+  const jsonData = await response.json();
+
+  const { products } = jsonData;
+  products.map((fetchedProduct) => {
+    appendFetchedProduct(fetchedProduct, fetchedProduct.id);
+    fetchedProductsMap.set(fetchedProduct.id, fetchedProduct);
+  });
+});
+
+function initializeDisplay() {
+  array.forEach((element) => {});
+  appendManufacturerToCart(cartProducts);
+  appendProductToManufacturer();
+}
+
+function mapToJSON(map) {
+  return JSON.stringify(Object.fromEntries(map));
+}
+
+function JSONtoMap(json) {
+  const obj = JSON.parse(json);
+  return new Map(Object.entries(obj));
+}
+
+function setToJSON(set) {
+  return JSON.stringify(Array.from(set));
+}
+
+function JSONtoSet(json) {
+  return new Set(JSON.parse(json));
+}
 
 const getCurrentProduct = (id) => {
   return cartProducts.filter((product) => product.id === Number(id))[0];
@@ -218,17 +271,6 @@ function deleteProduct(e, id) {
   console.log(cartProducts);
 }
 
-window.addEventListener("load", async (e) => {
-  const response = await fetch("https://dummyjson.com/products");
-  const jsonData = await response.json();
-
-  const { products } = jsonData;
-  products.map((fetchedProduct) => {
-    appendFetchedProduct(fetchedProduct, fetchedProduct.id);
-    fetchedProductsMap.set(fetchedProduct.id, fetchedProduct);
-  });
-});
-
 function appendFetchedProduct(fetchedProduct, id) {
   const shopContent = document.querySelector(".content__Shop");
   const product = document.createElement("div");
@@ -363,6 +405,7 @@ function appendManufacturerToCart(cartProducts) {
   displayedCartProducts = cartProducts.map(({ id, brand }) => {
     const manufacturerBox = document.createElement("div");
     manufacturerBox.className = `wrapper__Manufacturer__${brand}`;
+
     getBrandTotal(brand);
     const brandTotal = brandTotalMap.get(brand);
 
@@ -378,6 +421,7 @@ function appendManufacturerToCart(cartProducts) {
         Total:<input disabled class="total__Manufacturer__${brand}" value=${brandTotal} style="width:50px"/>
       </div>
     `;
+
     if (!brandSet.has(brand)) {
       cartContent.appendChild(manufacturerBox);
       brandSet.add(brand);
@@ -432,8 +476,12 @@ function appendProductToManufacturer(cartProducts) {
         productSet.add(Number(id));
       } else {
         let oldProduct = document.querySelector(`.wrapper__Product__Cart__${id}`);
-        oldProduct.remove();
-        manufacturerBox.appendChild(manufacturerProduct);
+        if (oldProduct) {
+          oldProduct.remove();
+        }
+        if (manufacturerBox) {
+          manufacturerBox.appendChild(manufacturerProduct);
+        }
       }
       const currentCheckbox = document.querySelector(`.checkbox__Product__${id}`);
       currentCheckbox.checked = isChecked;
@@ -482,36 +530,84 @@ function ProductCheckboxHandler(id) {
 function BrandCheckboxHandler(id) {
   const brand = trimSpecialCharacters(trimWhiteSpace(fetchedProductsMap.get(id).brand));
   const brandCheckboxArray = document.getElementsByClassName(`checkbox__Product__${brand}`);
+  const manufacturerTotal = document.querySelector(`.total__Manufacturer__${brand}`);
+
   for (let productCheckbox of brandCheckboxArray) {
     productCheckbox.checked = false;
   }
 
   if (isBrandCheckedSet.has(brand)) {
-    isBrandCheckedSet.delete(brand);
     const newCartProducts = [...cartProducts];
     newCartProducts.map((product) => {
       if (product.brand === brand) {
-        isChecked = false;
+        product.isChecked = false;
       }
+      return product;
     });
+
+    function newTotalDisplay(newBrandTotal) {
+      const wrapperManufacturerTotal = document.querySelector(`.manufacturer__Total__${brand}`);
+      const newTotalDisplay = document.createElement("input");
+      newTotalDisplay.disabled = true;
+      newTotalDisplay.className = `total__Manufacturer__${brand}`;
+      newTotalDisplay.value = newBrandTotal;
+      newTotalDisplay.style = "width:50px;";
+      if (wrapperManufacturerTotal) {
+        wrapperManufacturerTotal.appendChild(newTotalDisplay);
+      }
+    }
+    manufacturerTotal.remove();
+    let newBrandTotal = getBrandTotal(brand);
+    newTotalDisplay(newBrandTotal);
+    updateGrandTotal();
+    isBrandCheckedSet.delete(brand);
   } else {
     isBrandCheckedSet.add(brand);
-  }
-  const isBrandChecked = isBrandCheckedSet.has(brand);
-  const newCartProducts = [...cartProducts];
-  newCartProducts.map((product) => {
-    if (product.brand === brand) {
-      product.isChecked = isBrandChecked;
-    }
-    return product;
-  });
+    let newCartProducts = [...cartProducts];
+    newCartProducts.map((product) => {
+      if (product.brand === brand) {
+        product.isChecked = true;
+      }
+      return product;
+    });
+    const currentBrandTotal = brandTotalMap.get(brand);
+    let newBrandTotal = getBrandTotal(brand);
 
-  cartProducts = [...newCartProducts];
+    //TODO brand checkbox logic
+
+    function newTotalDisplay(newBrandTotal) {
+      const wrapperManufacturerTotal = document.querySelector(`.manufacturer__Total__${brand}`);
+      const newTotalDisplay = document.createElement("input");
+      newTotalDisplay.disabled = true;
+      newTotalDisplay.className = `total__Manufacturer__${brand}`;
+      newTotalDisplay.value = newBrandTotal;
+      newTotalDisplay.style = "width:50px;";
+      if (wrapperManufacturerTotal) {
+        wrapperManufacturerTotal.appendChild(newTotalDisplay);
+      }
+    }
+    manufacturerTotal.remove();
+    newTotalDisplay(newBrandTotal);
+  }
+
+  const currentProduct = getCurrentProduct(id);
 
   console.log(cartProducts);
-
-  getBrandTotal();
-  const currentProduct = getCurrentProduct(id);
-  updateBrandTotal(id, brand, currentProduct.price, currentProduct.count, currentProduct.isChecked);
-  updateGrandTotal();
+  console.log(brandTotalMap.get(brand));
 }
+
+window.addEventListener("unload", async (e) => {
+  const fetchedProductsMapJSON = mapToJSON(fetchedProductsMap);
+  const brandSetJSON = setToJSON(brandSet);
+  const productSetJSON = setToJSON(productSet);
+  const cartProductsJSON = JSON.stringify(cartProducts);
+  const brandTotalMapJSON = mapToJSON(brandTotalMap);
+  const isBrandCheckedSetJSON = setToJSON(isBrandCheckedSet);
+
+  await window.localStorage.setItem("fetchedProductsMapJSON", fetchedProductsMapJSON);
+  await window.localStorage.setItem("brandSetJSON", brandSetJSON);
+  await window.localStorage.setItem("productSetJSON", productSetJSON);
+  await window.localStorage.setItem("cartProductsJSON", cartProductsJSON);
+  await window.localStorage.setItem("brandTotalMapJSON", brandTotalMapJSON);
+  await window.localStorage.setItem("isBrandCheckedSetJSON", isBrandCheckedSetJSON);
+});
--- utils.js
+++ utils.js
@@ -2,6 +2,3 @@ export default const trimWhiteSpace = (string) => {
   return string.replace(/\s/g, "");
 };
 
-export const trimSpecialCharacters = (string) => {
-  return string.replace(/^a-zA-Z0-9 ]/g, "").replace(/[&-']/g, "");
-};
