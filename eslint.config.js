697f786c5510c752c1fc7dffa9ca2553670e67d8
Marcin Grzmil <marcin.grzmil@hotmail.com>
2024-09-18 23:30:28 +0200

    issue13 add parcel (#27)

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,24 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import pluginJest from 'eslint-plugin-jest'
 
 export default [
-    { languageOptions: { globals: globals.browser } },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.jest,
+            },
+        },
+    },
     pluginJs.configs.recommended,
+    {
+        plugins: {
+            jest: pluginJest,
+        },
+        rules: {
+            ...pluginJest.configs.recommended.rules,
+        },
+    },
 ]
